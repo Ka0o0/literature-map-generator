795ea1889718aa6dc8e700836ff353a734ed98f1
Kai Takac <takac.kai@fronius.com>
2019-08-09 11:31:13 +0200

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# literature-map-generator
-Generate a Markdown table from your biblatex bibliography 
+# Biblatex Markdown Table Generator
+
+Generate a grouped Markdown table from your biblatex bibliography.
+
+## Features
+
+- Automatically parse your .bib file
+- Generate a Markdown table
+- Group the literature by a custom key
+- Optionally create a link to your literature document if it is stored in **Jabref** style (file key)
+
+## Usage
+
+Simple run the program as following
+
+```
+program.jar inputFilePath groupingKey literatureRootPath
+```
+
+## Example
+
+**Note:** Links are broken on purpose
+
+
+| Literature Scope | Literatue |
+| --- | --- |
+| **PID in Something** |   |
+|   | [Potential Induced Degradatioon of Solar Cells and Panels](./files/Pingel2010%20-%20Potential%20Induced%20Degradatioon%20of%20Solar%20Cells%20and%20Panels.pdf) |
+| **N/A** |   |
+|   | [CRYSTALLINE SI SOLAR CELLS AND MODULES FEATURING EXCELLENT STABILITY AGAINST POTENTIAL-INDUCED DEGRADATION](./files/Nagel2011%20-%20CRYSTALLINE%20SI%20SOLAR%20CELLS%20aND%20MODULES%20FEATURING%20EXCELLENT%20STABILITY%20aGAINST%20POTENTIAL%20INDUCED%20DEGRADATION.pdf) |
+|   | [Comparison of Analysis Methods for the Calculation of Degredation Rates of Different Photovoltaic Technologies](./files/Phinikarides2013%20-%20Comparison%20of%20Analysis%20Methods%20for%20the%20Calculation%20of%20Degredation%20Rates%20of%20Different%20Photovoltaic%20Technologies.pdf) |
+|   | [Early degradation of silicon PV modules and guaranty conditions](./files/Munoz2011%20-%20Early%20Degradation%20of%20Silicon%20PV%20Modules%20and%20Guaranty%20Conditions.pdf) |
---
+++ src/main/java/LiteratureMapGenerator.java
@@ -0,0 +1,133 @@
+import org.jbibtex.*;
+
+import javax.swing.plaf.synth.SynthTabbedPaneUI;
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LiteratureMapGenerator {
+
+    static class LiteratureMapEntry implements Comparable<LiteratureMapEntry> {
+        String literatureName;
+        String fileName;
+
+        String generateMarkdownLink(String documentRootDirectory) {
+            if (fileName == null) {
+                return literatureName;
+            }
+
+            Pattern pattern = Pattern.compile(":(.*?):");
+            Matcher matcher = pattern.matcher(fileName);
+
+            if (!matcher.find()) {
+                return literatureName;
+            }
+
+            var encodedFileName = encodeURIComponent(matcher.group(1));
+            var fullFilePath = documentRootDirectory + encodedFileName;
+            return "[" + literatureName + "](" + fullFilePath + ")";
+        }
+
+        LiteratureMapEntry(String literatureName, String fileName) {
+            assert literatureName != null;
+            this.literatureName = literatureName;
+            this.fileName = fileName;
+        }
+
+        @Override
+        public int compareTo(LiteratureMapEntry o) {
+            return this.literatureName.compareTo(o.literatureName);
+        }
+    }
+
+    public static void main(String[] args) throws ParseException, IOException {
+        if (args.length != 3) {
+            System.err.println("Call program as following:");
+            System.err.println("program.jar inputFilePath groupingKey literatureRootPath");
+            System.exit(-1);
+        }
+        var literatureRootFolder = args[2];
+        var pathToBib = args[0];
+        var groupingKeyString = args[1];
+        var groupingKey = new Key(groupingKeyString);
+        var fileKey = new Key("file");
+        var naKeyString = "N/A";
+        var fr = new FileReader(pathToBib);
+
+        var bibtexParser = new BibTeXParser();
+        var database = bibtexParser.parse(fr);
+        fr.close();
+
+        var entries = database.getEntries();
+
+        var literatureMap = new TreeMap<String, SortedSet<LiteratureMapEntry>>();
+
+        for (BibTeXEntry bibTeXEntry : entries.values()) {
+            Value belongingGroup = bibTeXEntry.getField(groupingKey);
+            String belongingGroupString = naKeyString;
+
+            Value file = bibTeXEntry.getField(fileKey);
+            Value title = bibTeXEntry.getField(BibTeXEntry.KEY_TITLE);
+
+            if (title == null) {
+                System.err.println("No title found");
+                continue;
+            }
+
+            if (belongingGroup == null) {
+                System.err.println("No belonging group key found for literature: " + title.toUserString());
+            } else {
+                belongingGroupString = belongingGroup.toUserString();
+            }
+
+            if (!literatureMap.containsKey(belongingGroupString)) {
+                literatureMap.put(belongingGroupString, new TreeSet<>());
+            }
+
+            String fileName = file != null ? file.toUserString() : null;
+
+            literatureMap.get(belongingGroupString).add(new LiteratureMapEntry(title.toUserString(), fileName));
+        }
+
+        StringBuilder markdownTableSB = new StringBuilder();
+        markdownTableSB.append("| Literature Scope | Literatue |\n");
+        markdownTableSB.append("| --- | --- |\n");
+
+        for (String literatureMapKey : literatureMap.keySet()) {
+            if (literatureMapKey.equals(naKeyString)) {
+                continue;
+            }
+            appendMarkdownTableRowToLiteratureMapTable(literatureMapKey, literatureMap.get(literatureMapKey), markdownTableSB, literatureRootFolder);
+        }
+
+        if (literatureMap.containsKey(naKeyString)) {
+            appendMarkdownTableRowToLiteratureMapTable(naKeyString, literatureMap.get(naKeyString), markdownTableSB, literatureRootFolder);
+        }
+
+        System.out.println(markdownTableSB.toString());
+    }
+
+    private static void appendMarkdownTableRowToLiteratureMapTable(String literatureMapKey, SortedSet<LiteratureMapEntry> literatureMapEntries, StringBuilder markdownTableSB, String literatureRootFolder) {
+        markdownTableSB.append("| **").append(literatureMapKey).append("** |   |\n");
+        for (LiteratureMapEntry literatureMapEntry : literatureMapEntries) {
+            markdownTableSB.append("|   | ").append(literatureMapEntry.generateMarkdownLink(literatureRootFolder)).append(" |\n");
+        }
+    }
+
+    private static String encodeURIComponent(String s) {
+        String result;
+
+        result = URLEncoder.encode(s, StandardCharsets.UTF_8)
+                .replaceAll("\\+", "%20")
+                .replaceAll("\\%21", "!")
+                .replaceAll("\\%27", "'")
+                .replaceAll("\\%28", "(")
+                .replaceAll("\\%29", ")")
+                .replaceAll("\\%7E", "~");
+
+        return result;
+    }
+}
